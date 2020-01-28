96013c1fd361e06037fdb1b0e2ef4bd8fc9830a8
ktalant <uchkunfan@gmail.com>
2020-01-28 13:48:07 -0500

    security group has been created

--- main.tf
+++ main.tf
@@ -189,3 +189,63 @@ resource "aws_security_group" "wp_bastion_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+# Public security group
+resource "aws_security_group" "wp_public_sg" {
+    name = "wp-public-SG"
+    description = "Used for elastic load balancer for public access"
+    vpc_id = aws_vpc.wp_vpc.id
+
+    # Http
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Private security group
+resource "aws_security_group" "wp_private_sg" {
+    name = "wp-private-SG"
+    description = "Used for private connections"
+    vpc_id = aws_vpc.wp_vpc.id
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [var.vpc_cidr]
+    }
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# RDS security group
+resource "aws_security_group" "wp_rd_sg" {
+    name = "wp-rds-SG"
+    description = "Used for RDS instances"
+    vpc_id = aws_vpc.wp_vpc.id
+
+    # SQL access from public and private security groups
+    ingress {
+      from_port = 3306
+      to_port = 3306
+      protocol = "tcp"
+      security_groups = [aws_security_group.wp_public_sg.id,
+                         aws_security_group.wp_private_sg.id,
+                         aws_security_group.wp_bastion_sg.id]
+    }
+}
