5b425575b6acc8f89e5722bb7c26146abbf4a2b7
ktalant <uchkunfan@gmail.com>
2020-01-27 23:09:50 -0500

    updated

--- main.tf
+++ main.tf
@@ -32,13 +32,15 @@ resource "aws_iam_role" "s3_access_role" {
     {
       "Version": "2012-10-17",
       "Statement": [
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
       ]
     }
 EOF
-}  
+}
