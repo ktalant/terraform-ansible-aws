ac17648e77dd5799fae4bddc8b54b341b88bd70e
ktalant <uchkunfan@gmail.com>
2020-01-27 23:36:41 -0500

    policy has been updated

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_iam_instance_profile" "s3_access_profile" {
 resource "aws_iam_role_policy" "s3_access_policy" {
     name = "s3_access_policy"
     role = aws_iam_role.s3_access_role.id
+    
     policy = <<EOF
     {
       "Version": "2012-10-17",
