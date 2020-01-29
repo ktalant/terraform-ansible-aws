93e1e08985d09b126756e8eb927b925b977e3105
ktalant <uchkunfan@gmail.com>
2020-01-28 23:10:41 -0500

    file has been updated

--- main.tf
+++ main.tf
@@ -255,7 +255,7 @@ resource "aws_vpc_endpoint" "wp_private-s3_endpoint" {
     vpc_id = aws_vpc.wp_vpc.id
     service_name = "com.amazonaws.${var.aws_region}.s3"
     route_table_ids = [aws_vpc.wp_vpc.main_route_table_id,
-      aws_vpc.wp_vpc.wp_public_rt.id]
+      aws_route_table.wp_public_rt.id]
     policy = <<-POLICY
     {
       "Statement": [
